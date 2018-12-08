18c093c928fdb41419f3b5005b20d4d2910e4698
Tamir <tamiradimor@gmail.com>
2018-12-08 12:31:31 +0200

    javascript response

---
+++ src/signUp/signUpForm.js
@@ -0,0 +1,8 @@
+import currencies from "../../config/currency.json"
+
+
+for (let currency in currencies) {
+    let element = document.createElement("a");
+    element.innerHTML = currency.name + " - " + currency.cone;
+    document.getElementById("currency-dropdown").appendChild(element);
+}
