b058e02f4f296d3f3ac65cb556df44a1e38d3847
edencohen94 <Eden84951834>
2018-12-15 12:40:51 +0200

    commit

---
+++ src/homePage/calculator.js
@@ -0,0 +1,4 @@
+<!--Begin Currency Converter Code-->
+<div id="FEXRdivResp" data-pym-src="//www.foreignexchangeresource.com/currency-converter.php?c=EUR&a=USD&amt=1&panel=1&button=2&headertxtcolor=3e3e3e&resulttxtcolor=000085&bgcolor=ffffff&bordercolor=dddddd&titleboxtop=f5f5f5&titleboxbottom=e8e8e8&corners=&fontstyle=Arial%2C+Helvetica%2C+sans-serif"></div>
+<script type="text/javascript" src="//www.foreignexchangeresource.com/js/pym.min.js"></script>
+<!--End Currency Converter Code-->
