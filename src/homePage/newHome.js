2d74034876815150bd0037ad045f8b853ab2c11e
edencohen94 <Eden84951834>
2018-12-20 16:25:39 +0200

    commit

--- src/homePage/newHome.js
+++ src/homePage/newHome.js
@@ -8,10 +8,29 @@ for (let offer of allOffers.offers) {
     let preferred = $("<a></a>").text(offer.preferredCurr);
     let city = $("<a></a>").text(offer.city);
     let lastUpdate = $("<a></a>").text(offer.lastUpdate);
+    var result= $( ".topResults" ).append( "<div class = 'result container'></div>");
+    $('<div>', {id: 'amount'+i, class: 'amount'}).appendTo(result);
+    $('<div>', {id: 'currency'+i,class: 'currency'}).appendTo(result);
+    $('<div>', {id: 'preferred'+i, class: 'Pcurrency'}).appendTo(result);
+    $('<div>', {id: 'city'+i, class:'city' }).appendTo(result);
+    $('<div>', {id: 'lastUpdate'+i, class: 'lastUpdate'}).appendTo(result);
     $("#amount"+i).append(amount);
     $("#currency"+i).append(curr);
     $("#preferred"+i).append(preferred);
     $("#city"+i).append(city);
     $("#lastUpdate"+i).append(lastUpdate);
-    i=i+1;
+    //$('<div class="amount"></div>')
+    //    .attr("id", "amount" + i)
+      //  .appendTo(".result container");
+    //$('<div class="currency"></div>')
+     //   .attr("id", "currency" + i)
+       // .appendTo(".result container");
+    //$('<div class="Pcurrency"></div>')
+      //  .attr("id", "preferred" + i)
+        //.appendTo(".result container");
+    //$('<div class="city"></div>')
+      //  .attr("id", "city" + i)
+        //.appendTo(".result container");
+    //$('<div class="lastUpdate"></div>')
+      //  .attr("id", "lastUpdate" + i)
 }
