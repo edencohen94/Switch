247c4c37557f0aab1eddae6afb29fc07e0fb26ed
edencohen94 <Eden84951834>
2018-12-20 17:14:15 +0200

    commit

--- src/homePage/newHome.js
+++ src/homePage/newHome.js
@@ -3,6 +3,7 @@ $('.nav-tabs a:first').tab('show')
 
 var i=1;
 for (let offer of allOffers.offers) {
+    let check = $("<input type=\"checkbox\" id=\"chk4\" className=\"checkbox\"/>").addClass("checkbox");
     let amount = $("<a></a>").text(offer.amount);
     let curr = $("<a></a>").text(offer.currency);
     let preferred = $("<a></a>").text(offer.preferredCurr);
@@ -10,30 +11,19 @@ for (let offer of allOffers.offers) {
     let lastUpdate = $("<a></a>").text(offer.lastUpdate);
     var el= $( "<div class = 'result container'></div>");
     var result= $( ".topResults" ).append(el);
+    $('<div>', {id: 'check'+i}).appendTo(el);
     $('<div>', {id: 'amount'+i, class: 'amount'}).appendTo(el);
     $('<div>', {id: 'currency'+i,class: 'currency'}).appendTo(el);
     $('<div>', {id: 'preferred'+i, class: 'Pcurrency'}).appendTo(el);
     $('<div>', {id: 'city'+i, class:'city' }).appendTo(el);
     $('<div>', {id: 'lastUpdate'+i, class: 'lastUpdate'}).appendTo(el);
+    $("#check"+i).append(check);
     $("#amount"+i).append(amount);
     $("#currency"+i).append(curr);
     $("#preferred"+i).append(preferred);
     $("#city"+i).append(city);
     $("#lastUpdate"+i).append(lastUpdate);
-    //$('<div class="amount"></div>')
-    //    .attr("id", "amount" + i)
-      //  .appendTo(".result container");
-    //$('<div class="currency"></div>')
-     //   .attr("id", "currency" + i)
-       // .appendTo(".result container");
-    //$('<div class="Pcurrency"></div>')
-      //  .attr("id", "preferred" + i)
-        //.appendTo(".result container");
-    //$('<div class="city"></div>')
-      //  .attr("id", "city" + i)
-        //.appendTo(".result container");
-    //$('<div class="lastUpdate"></div>')
-      //  .attr("id", "lastUpdate" + i)
 
+    i=i+1;
 
 }
