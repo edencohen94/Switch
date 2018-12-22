2b3f457fedf09980808efe3cb51ffdfaa91098f5
Tamir <tamiradimor@gmail.com>
2018-12-22 02:03:33 +0200

    improving offers page

--- src/openOffers/openOffers.js
+++ src/openOffers/openOffers.js
@@ -49,22 +49,18 @@ let state = {
 $(".greetings").text(state.username + "'s offers");
 
 for (let offer of state.offers) {
-    // let card = $("<div class=\"card\">\n" +
-    //     "  <h5 class=\"card-header\">Featured</h5>\n" +
-    //     "  <div class=\"card-body\">\n" +
-    //     "    <h5 class=\"card-title\">Special title treatment</h5>\n" +
-    //     "    <p class=\"card-text\">With supporting text below as a natural lead-in to additional content.</p>\n" +
-    //     "    <a href=\"#\" class=\"btn btn-primary\">Go somewhere</a>\n" +
-    //     "  </div>\n" +
-    //     "</div>");
-
     let cardBody = $("<div></div>", {class: "card-body info-container"})
         .append($("<span></span>", {class: "offer-detail"}).text("Amount: " + offer.amount))
         .append($("<span></span>", {class: "offer-detail"}).text("Date: " + offer.date))
         .append($("<span></span>", {class: "offer-detail"}).text("City: " + offer.city));
+
+    let cardButtons = $("<div></div>", {class: "ad-action-container"})
+        .append($("<button></button>", {class: "btn btn-danger cancel-changes card-button"}).text("Delete"));
+
     let card = $("<div></div>", {class: "card offer-card"})
         .append($("<h5></h5>", {class: "card-header"}).text(offer.currency))
-        .append(cardBody);
+        .append(cardBody)
+        .append(cardButtons);
 
     $(".offers-container").append(card);
 }
