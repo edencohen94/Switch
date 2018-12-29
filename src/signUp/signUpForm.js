287f898e19afdfbd31e056e11cd042e36cf23e0d
edencohen94 <Eden84951834>
2018-12-29 22:05:00 +0200

    commit

--- src/signUp/signUpForm.js
+++ src/signUp/signUpForm.js
@@ -16,6 +16,7 @@ function preferredCurrency() {
     preferred1 = $(this).val();
     $("#dropdownMenuOffset1").html(preferred1);
 };
+
 $("#currency-dropdown2").click(function () {
     preferred2 = (event.target).text;
     $("#dropdownMenuOffset2").html(preferred2);
